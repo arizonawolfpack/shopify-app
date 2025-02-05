d6539ba24e414c9d551a88bdb12ed68544c7864f
arizonawolfpack <shawnsampson92@gmail.com>
2025-02-05 02:03:30 -0800

    Delete src/routes/src/customerService.js

--- src/routes/src/customerService.js
+++
@@ -1,7 +0,0 @@
-export async function getCustomerData(customerId) {
-    // TODO: Replace with actual database call
-    return {
-        purchaseHistory: ['Product A', 'Product B'],
-        totalSpent: 300,
-    };
-}
