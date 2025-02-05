7fb53bd1d23043cc75ad0364bf76714dc800e8fa
arizonawolfpack <shawnsampson92@gmail.com>
2025-02-05 02:07:10 -0800

    customerService.js

---
+++ src/customerService.js
@@ -0,0 +1,7 @@
+export async function getCustomerData(customerId) {
+    // TODO: Replace with actual database call
+    return {
+        purchaseHistory: ['Product A', 'Product B'],
+        totalSpent: 300,
+    };
+}
